be3724f4641617f22be5579f291f065ac9ba7d1f
Will Hodgson <will.hodgson@current-rms.com>
2019-10-02 08:28:48 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,9 +3,9 @@ class Pokemon
   @@all = []
 
   def initialize(db)
-    # @id = id
-    # @name = name
-    # @type = type
+    @id = id
+    @name = name
+    @type = type
     @db = db
     @@all << self
   end
